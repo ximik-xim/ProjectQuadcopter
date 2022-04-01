b19ddfb050595e75989e63fa68d28d942f8ce353
Matroskin_Cat <groznov3@gmail.com>
2022-04-01 11:22:28 +0300

    Спроектирована база для сущностей

---
+++ Assets/Scripts/Actors/ReactableEntity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public abstract class ReactableEntity : MonoBehaviour
+    {
+        protected virtual void FlyAway()
+        {
+
+        }
+
+        protected virtual void KnockOff()
+        {
+
+        }
+
+        protected virtual void Catch()
+        {
+
+        }
+
+        protected virtual void TakeHalfHP()
+        {
+
+        }
+
+        protected virtual void LeanOutTheWindow()
+        {
+
+        }
+    }
+}
