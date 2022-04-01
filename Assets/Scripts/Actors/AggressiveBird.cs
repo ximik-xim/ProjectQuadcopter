aee7794ffb1c693cbe26a43745f672b7fb258df9
Matroskin_Cat <groznov3@gmail.com>
2022-04-01 11:25:34 +0300

    Добавлены шаблоны сущностей
    - Машина
    - Сачок
    - Агрессивная птица
    - Бельевая верёвка

---
+++ Assets/Scripts/Actors/AggressiveBird.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class AggressiveBird : ReactableEntity, ICollisionReactable
+    {
+        protected override void TakeHalfHP() => base.TakeHalfHP();
+
+        public void OnCollisionEnter(Collision collision) => base.TakeHalfHP();
+    }
+}
