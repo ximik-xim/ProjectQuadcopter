7a08b3ca4dcec6e0dceb6901f6ebc2df0bb59b5a
Matroskin_Cat <groznov3@gmail.com>
2022-04-05 21:14:19 +0300

    Обновление сервиса update'ов

--- Assets/Scripts/Services/UpdateService.cs
+++ Assets/Scripts/Services/UpdateService.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class UpdateService : MonoBehaviour
+public sealed class UpdateService : MonoBehaviour
 {
     public static Action OnUpdate;
     public static Action OnFixedUpdate;
