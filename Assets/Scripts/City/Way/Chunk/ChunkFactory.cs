349b261c96326f9433819130adfcc86d9b9cb5bb
Matroskin_Cat <groznov3@gmail.com>
2022-04-05 21:06:41 +0300

    Необходимые скрипты для чанков

---
+++ Assets/Scripts/City/Way/Chunk/ChunkFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ChunkFactory : IFactory<Chunk>
+    {
+        private List<Chunk> _prefabs;
+        private Container _container;
+
+        public ChunkFactory(List<Chunk> prefabs, Container container)
+        {
+            _prefabs = prefabs;
+            _container = container;
+        }
+
+        public Chunk GetCreated() => Object.Instantiate(_prefabs[Random.Range(0, _prefabs.Count)], _container.transform);
+    }
+}
