a623ee3133d69c75354f7ede158aab43f393cdf2
Matroskin_Cat <groznov3@gmail.com>
2022-04-06 22:39:22 +0300

    Обновление генератора пути

---
+++ Assets/Scripts/City/Way/WayGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WayGenerator : MonoBehaviour
+    {
+        private Coroutine _loopRoutine;
+        private ChunkFactory _chunkFactory;
+        private Pool<Chunk> _chunksPool;
+
+        public void StartLoop() => _loopRoutine = StartCoroutine(LoopRoutine());
+
+        public void GenerateChunks(List<Chunk> prefabs, Container container, int amount)
+        {
+            _chunkFactory = new ChunkFactory(prefabs, container);
+            _chunksPool.Init(_chunkFactory, container, amount);
+        }
+
+        private IEnumerator LoopRoutine()
+        {
+            float spawnTemp = 1.5f;
+
+            while (true)
+            {
+                _chunksPool.Get();
+                yield return new WaitForSeconds(spawnTemp * SpeedProvider.Speed);
+            }
+        }
+
+        private void OnDisable() => StopCoroutine(_loopRoutine);
+    }
+}
