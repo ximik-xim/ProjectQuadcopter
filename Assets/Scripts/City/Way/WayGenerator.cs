62ea2deceecce93de59999bb15ed574a5e469da1
Matroskin_Cat <groznov3@gmail.com>
2022-04-08 19:30:26 +0300

    Обновлён генератор пути

--- Assets/Scripts/City/Way/WayGenerator.cs
+++ Assets/Scripts/City/Way/WayGenerator.cs
@@ -9,19 +9,26 @@ namespace Assets.Scripts
     {
         private ChunkFactory _chunkFactory;
         private Pool<Chunk> _chunksPool;
+        private WayMatrix _wayMatrix = new WayMatrix();
+        private float _chunkSize;
 
         public void InitWay(List<Chunk> prefabs, Container container, int amount)
         {
-            _chunkFactory = new ChunkFactory(prefabs, container, SpawnChunk);
+            _chunkFactory = new ChunkFactory(prefabs, container, SpawnChunk, out _chunkSize);
             _chunksPool.Init(_chunkFactory, container, amount);
-            SpawnStartChunksToHorizon();
         }
 
-        private void SpawnStartChunksToHorizon()
+        public void SpawnStartableChunks(int amount)
         {
+            float spacing = 0;
 
+            for (int i = 0; i < amount; i++)
+            {
+                _chunksPool.Get(new Vector3(_wayMatrix.Horizon.x, _wayMatrix.Horizon.y, _wayMatrix.Horizon.z + spacing));
+                spacing -= _chunkSize;
+            }
         }
 
-        public void SpawnChunk() => _chunksPool.Get();
+        public void SpawnChunk() => _chunksPool.Get(_wayMatrix.Horizon);
     }
 }
