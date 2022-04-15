9a9b46fd99678ca6ca9267bbd55027435b070141
Matroskin_Cat <groznov3@gmail.com>
2022-04-15 15:01:54 +0300

    Обновление General

--- Assets/Scripts/General/GameStartup.cs
+++ Assets/Scripts/General/GameStartup.cs
@@ -5,9 +5,9 @@ namespace Assets.Scripts
 {
     public class GameStartup : MonoBehaviour
     {
-        [SerializeField] private Transform _city;
-        [SerializeField] private SpeedProvider _speedProvider;
-        [SerializeField] private WayGenerator _wayGenerator;
+        [SerializeField] private City _city;
+        [SerializeField] private WayMatrix _wayMatrix;
+        [SerializeField] private ChunkGenerator _chunkGenerator;
 
         [Header("Prefabs")]
         [SerializeField] private Quadcopter _quadcopterPrefab;
@@ -20,31 +20,18 @@ namespace Assets.Scripts
         [SerializeField] private List<Chunk> _chunkPrefabs;
 
         [Header("Configurations")]
-        [SerializeField] [Range(.1f, 10)] private float _startSpeed;
+        [SerializeField] [Range(10, 100)] private float _startSpeed;
 
-        private WayMatrix _wayMatrix = new WayMatrix();
-
-        private Container _chunksContainer;
+        
         private Container _entitieContainer;
 
         private void Start()
         {
-            _chunksContainer = GetCreatedContainer("ChunksContainer");
-            _entitieContainer = GetCreatedContainer("EntityContainer");
-            _speedProvider.SetStartSpeed(_startSpeed);
-            _wayGenerator.InitWay(_chunkPrefabs, _chunksContainer, _chunkPrefabs.Count); // Õ¿–”ÿ≈Õ¿ »Õ ¿œ—”Àﬂ÷»ﬂ CHUNK PREFABS !!!
-            _wayGenerator.SpawnStartableChunks(4);
-            GetCreatedActor(new PlayerCameraFactory(_playerCameraPrefab, _entitieContainer, _wayMatrix.GetPosition(MatrixPosition.Center)));
-            GetCreatedActor(new QuadcopterFactory(_quadcopterPrefab, _entitieContainer, _wayMatrix.GetPosition(MatrixPosition.Center)));
-        }
-
-        private Container GetCreatedContainer(string title)
-        {
-            GameObject container = new GameObject(title);
-            container.transform.SetParent(_city);
-            container.transform.position = _wayMatrix.Horizon;
-            container.AddComponent(typeof(Container));
-            return container.GetComponent<Container>();
+            _entitieContainer = ContainerService.GetCreatedContainer("EntityContainer", _city.transform, Vector3.zero);
+            GetCreatedActor(new QuadcopterFactory(_quadcopterPrefab, _entitieContainer, _wayMatrix));
+            GetCreatedActor(new PlayerCameraFactory(_playerCameraPrefab, _entitieContainer, _wayMatrix.Center));
+            SpeedService.SetStartSpeed(_startSpeed);
+            _chunkGenerator.Init(_city, _wayMatrix, _chunkPrefabs, 5);
         }
 
         private T GetCreatedActor<T>(IFactory<T> factory) where T : Actor => factory.GetCreated();
