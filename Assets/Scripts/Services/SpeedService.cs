9d50b204c9d0e06d6a59b0dbcd0af6bb53a8e4ff
Matroskin_Cat <groznov3@gmail.com>
2022-04-15 15:02:59 +0300

    Обновление сервиса глобальной скорости

---
+++ Assets/Scripts/Services/SpeedService.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SpeedService : MonoBehaviour
+    {
+        private const float SpeedUpValue = 0.001f;
+
+        public static float Speed { get; private set; }
+
+        public static void SetStartSpeed(float startSpeed) => Speed = startSpeed;
+
+        private void OnEnable() => UpdateService.OnFixedUpdate += SpeedUp;
+
+        private void SpeedUp() => Speed += SpeedUpValue * Time.fixedDeltaTime;
+
+        private void OnDisable() => UpdateService.OnFixedUpdate -= SpeedUp;
+    }
+}
