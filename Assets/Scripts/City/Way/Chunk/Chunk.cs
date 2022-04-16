d9db3e672d0073851297264425b9e064f535a25e
Matroskin_Cat <groznov3@gmail.com>
2022-04-16 12:00:43 +0300

    Обновление чанков
    - Фабрика
    - База

--- Assets/Scripts/City/Way/Chunk/Chunk.cs
+++ Assets/Scripts/City/Way/Chunk/Chunk.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class Chunk : MonoBehaviour 
+    public class Chunk : Actor 
     {
-        
+        private List<SpawnPoint> _spawnPoints = new List<SpawnPoint>();
+
+        public float Size { get; private set; }
+
+        public override void Construct()
+        {
+            Size = GetComponentInChildren<MeshRenderer>().bounds.size.z;
+            _spawnPoints.AddRange(GetComponentsInChildren<SpawnPoint>());
+        }
+
+        public void GenerateWindows()
+        {
+
+        }
     }
 }
