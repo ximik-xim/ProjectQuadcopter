3d8e5dabe3d532c90eca29938888c7e716bfeb8b
Matroskin_Cat <groznov3@gmail.com>
2022-04-16 11:59:49 +0300

    Обновление архитектуры актёров

--- Assets/Scripts/City/Way/Actors/PlayerCamera/PlayerCamera.cs
+++ Assets/Scripts/City/Way/Actors/PlayerCamera/PlayerCamera.cs
@@ -1,4 +1,10 @@
 ﻿namespace Assets.Scripts
 {
-    public class PlayerCamera : Actor { }
+    public class PlayerCamera : Actor
+    {
+        public override void Construct()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
