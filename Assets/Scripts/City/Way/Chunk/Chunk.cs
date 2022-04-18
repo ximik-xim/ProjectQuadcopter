8bfeea03c2901d5f0c912c9d92ea1b43c908a068
Matroskin_Cat <groznov3@gmail.com>
2022-04-18 15:19:11 +0300

    Обновление класса чанк

--- Assets/Scripts/City/Way/Chunk/Chunk.cs
+++ Assets/Scripts/City/Way/Chunk/Chunk.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class Chunk : MonoBehaviour 
+    public class Chunk : Actor 
     {
+        public event SpawnHandler OnDisappear;
+
         private List<SpawnPoint> _spawnPoints = new List<SpawnPoint>();
 
         public float Size { get; private set; }
+        public Vector3 ConnectPosition { get; private set; }
 
-        public void Construct()
+        private void Awake()
         {
             Size = GetComponentInChildren<MeshRenderer>().bounds.size.z;
+            ConnectPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + Size);
             _spawnPoints.AddRange(GetComponentsInChildren<SpawnPoint>());
         }
 
