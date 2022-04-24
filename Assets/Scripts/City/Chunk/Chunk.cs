bbdce6cea1a1c52d960b0fbc74f725a67ef908c5
Matroskin_Cat <groznov3@gmail.com>
2022-04-24 15:08:27 +0300

    Обновление чанков
    - Фабрика
    - База

---
+++ Assets/Scripts/City/Chunk/Chunk.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Chunk : Actor 
+    {
+        private List<SpawnPoint> _spawnPoints = new List<SpawnPoint>();
+
+        public float Size { get; private set; }
+        public Vector3 ConnectPosition => new Vector3(transform.position.x, transform.position.y, transform.position.z + Size);
+
+        private void Awake()
+        {
+            Size = GetComponentInChildren<MeshRenderer>().bounds.size.z;
+            _spawnPoints.AddRange(GetComponentsInChildren<SpawnPoint>());
+        }
+
+        public void SetWindows()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/City/Chunk/ChunkFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ChunkFactory : IFactory<Chunk>
+    {
+        private List<Chunk> _prefabs;
+        private Container _container;
+        private int _prefabIndex = 0;
+        private WayMatrix _wayMatrix;
+        SpawnMethod _spawnMethod;
+
+        public ChunkFactory(IEnumerable<Chunk> prefabs, Container container, WayMatrix wayMatrix, SpawnMethod spawnMethod)
+        {
+            _prefabs = new List<Chunk>(prefabs);
+            _container = container;
+            _wayMatrix = wayMatrix;
+            _spawnMethod = spawnMethod;
+        }
+
+        public Chunk GetCreated()
+        {
+            _prefabIndex = (_prefabIndex == _prefabs.Count) ? 0 : _prefabIndex;
+            Chunk chunk = Object.Instantiate(_prefabs[_prefabIndex], _container.transform);
+            Disappearer disappearer = chunk.GetComponent<Disappearer>();
+            _prefabIndex++;
+            disappearer.OnDisappear += _spawnMethod;
+            disappearer.SetDisappearPoint(new Vector3(_wayMatrix.Center.x, _wayMatrix.Center.y, _wayMatrix.Center.z - chunk.Size));
+            return chunk;
+        }
+    }
+}
