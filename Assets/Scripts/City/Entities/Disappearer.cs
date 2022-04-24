b640bb645423fe233c195e6d355daa5b816fc6cf
Matroskin_Cat <groznov3@gmail.com>
2022-04-24 15:09:29 +0300

    Добавление компонента исчезания

---
+++ Assets/Scripts/City/Entities/Disappearer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Disappearer : MonoBehaviour
+    {
+        public event SpawnMethod OnDisappear;
+
+        private Vector3 _disappearPoint;
+
+        public void SetDisappearPoint(Vector3 disappearPoint) => _disappearPoint = disappearPoint;
+
+        private void OnEnable() => UpdateService.OnUpdate += CheckEdgeOut;
+
+        private void CheckEdgeOut()
+        {
+            if (transform.position.z <= _disappearPoint.z)
+            {
+                OnDisappear?.Invoke();
+                gameObject.SetActive(false);
+            }
+        }
+
+        private void OnDisable() => UpdateService.OnUpdate -= CheckEdgeOut;
+    }
+}
