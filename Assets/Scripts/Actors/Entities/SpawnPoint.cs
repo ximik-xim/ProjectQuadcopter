e5ab47aa7a2c956fb66ecfb65b0b495823ddcd48
DmitryV2044 <dvovchinskiy@inbox.ru>
2022-04-27 19:54:10 +0500

    Обновление спавнпоинита
    + Меняет скейл в зависимости от стороны
    
    Co-Authored-By: Matroskin_Cat <62561562+DarkGlasses13@users.noreply.github.com>

--- Assets/Scripts/Actors/Entities/SpawnPoint.cs
+++ Assets/Scripts/Actors/Entities/SpawnPoint.cs
@@ -2,5 +2,11 @@
 
 namespace Assets.Scripts
 {
-    public class SpawnPoint : MonoBehaviour { }
+    public class SpawnPoint : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (transform.position.x > 0) transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
 }
