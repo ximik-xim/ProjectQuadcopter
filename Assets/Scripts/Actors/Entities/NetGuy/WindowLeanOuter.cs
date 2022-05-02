ca0cd14f333e09c8831d9b950dff089b35f4cb24
Matroskin_Cat <groznov3@gmail.com>
2022-05-02 12:55:07 +0300

    Добавление компонента выскакивания из окна

---
+++ Assets/Scripts/Actors/Entities/NetGuy/WindowLeanOuter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WindowLeanOuter : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Awake() => _animator = GetComponent<Animator>();
+
+        public void LeanOutWindow()
+        {
+            _animator.SetFloat(AnimationService.Parameters.LeanOutingSide, - Mathf.Clamp(transform.position.x, -1, 1));
+        }
+
+        private void OnDisable() => _animator.SetFloat(AnimationService.Parameters.LeanOutingSide, 0);
+    }
+}
