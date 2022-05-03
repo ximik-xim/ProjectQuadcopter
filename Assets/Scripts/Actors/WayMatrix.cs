c67921db3f7bcfe54f7711dc8cc576a7ed81fb1a
MaminProgrammist <kit04048@gmail.com>
2022-05-03 23:07:04 +0300

    захуярим WayMatrix
    хуярил весь день как папа карло

--- Assets/Scripts/Actors/WayMatrix.cs
+++ Assets/Scripts/Actors/WayMatrix.cs
@@ -1,9 +1,10 @@
+using System;
 using UnityEngine;
 
 public class WayMatrix
 {
     private const int _width = 3;
-    private const int _height = 3;
+    private const int _height = 4;
     private const float _spacing = 4;
 
     private Vector2[,] _matrix;
@@ -14,6 +15,11 @@ public class WayMatrix
 
     public WayMatrix()
     {
+        if(Width != 3)
+        {
+            throw new Exception("BLAYT SHIRINA MATRIZZI NE RAVNA 3!!!!");
+        }
+
         _matrix = new Vector2[Height, Width];
         float xPositionValue = Spacing;
 
@@ -29,6 +35,8 @@ public class WayMatrix
 
             xPositionValue -= Spacing;
         }
+
+        //PrintMatrix();
     }
 
     public void PrintMatrix()
@@ -48,6 +56,47 @@ public class WayMatrix
         Debug.Log(matrixOut);
     }
 
+    public Vector3 GetPosition(MatrixPosition position, out Vector2Int worldMatrixCoordinates)
+    {
+        switch (position)
+        {
+            case MatrixPosition.UpLeft:
+                {
+                    Vector2Int matrixCoordinates = new Vector2Int(0, 0);
+                    worldMatrixCoordinates = ConvertCoordinates(matrixCoordinates);
+                    return _matrix[matrixCoordinates.x, matrixCoordinates.y];
+                }
+            case MatrixPosition.UpRight:
+                {
+                    Vector2Int matrixCoordinates = new Vector2Int(0, Width - 1);
+                    worldMatrixCoordinates = ConvertCoordinates(matrixCoordinates);
+                    return _matrix[matrixCoordinates.x, matrixCoordinates.y];
+                }
+            case MatrixPosition.Center:
+                {
+                    Vector2Int matrixCoordinates = new Vector2Int(Height - 2, Width / 2);
+                    worldMatrixCoordinates = ConvertCoordinates(matrixCoordinates);
+                    return _matrix[matrixCoordinates.x, matrixCoordinates.y];
+                }
+            case MatrixPosition.DownLeft:
+                {
+                    Vector2Int matrixCoordinates = new Vector2Int(Height - 1, 0);
+                    worldMatrixCoordinates = ConvertCoordinates(matrixCoordinates);
+                    return _matrix[matrixCoordinates.x, matrixCoordinates.y];
+                }
+            case MatrixPosition.DownRight:
+                {
+                    Vector2Int matrixCoordinates = new Vector2Int(Height - 1, Width - 1);
+                    worldMatrixCoordinates = ConvertCoordinates(matrixCoordinates);
+                    return _matrix[matrixCoordinates.x, matrixCoordinates.y];
+                }
+            default:
+                {
+                    goto case MatrixPosition.Center;
+                }
+        }
+    }
+
     public Vector3 GetPosition(MatrixPosition position)
     {
         switch (position)
@@ -62,7 +111,7 @@ public class WayMatrix
                 }
             case MatrixPosition.Center:
                 {
-                    return _matrix[Height / 2, Width / 2];
+                    return _matrix[Height - 2, Width / 2];
                 }
             case MatrixPosition.DownLeft:
                 {
@@ -79,7 +128,10 @@ public class WayMatrix
         }
     }
 
-    public Vector3 GetPosition(int x, int y) => _matrix[y, x];
+    public Vector3 GetCoordinatePosistion(Vector2Int position)
+    {
+        return _matrix[ConvertCoordinates(position).x, ConvertCoordinates(position).y];
+    }
 
     public Vector3[] GetRowWithIndex(int rowIndex)
     {
@@ -91,6 +143,11 @@ public class WayMatrix
         }
 
         return matrixRow;
+    }
+
+    private Vector2Int ConvertCoordinates(Vector2Int position)
+    {
+        return new Vector2Int(position.y, position.x);
     }
 }
 
