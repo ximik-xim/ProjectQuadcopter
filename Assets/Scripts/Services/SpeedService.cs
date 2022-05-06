7b0e787feb99da58d3397df42b774a95ace0c36e
MaminProgrammist <kit04048@gmail.com>
2022-05-06 17:57:09 +0300

    Пофиксил SpeedService
    - Переименовал ускорение
    - Добавил дельту

--- Assets/Scripts/Services/SpeedService.cs
+++ Assets/Scripts/Services/SpeedService.cs
@@ -4,15 +4,16 @@ namespace Assets.Scripts
 {
     public class SpeedService : MonoBehaviour
     {
-        private const float SpeedUpValue = 0.0005f;
+        //private const float SpeedUpValue = 0.05f;//0.0005f
 
         public static float Speed { get; private set; }
+        public static float Acceleration { get => 0.0005f; }
 
         public static void SetStartSpeed(float startSpeed) => Speed = startSpeed;
 
         private void OnEnable() => UpdateService.OnFixedUpdate += SpeedUp;
 
-        public static void SpeedUp() => Speed += SpeedUpValue;
+        public static void SpeedUp() => Speed += Acceleration * Time.fixedDeltaTime;
 
         private void OnDisable() => UpdateService.OnFixedUpdate -= SpeedUp;
     }
